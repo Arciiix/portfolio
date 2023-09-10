861a57b551025796b7d2658c244f39ac09dc9cfd
Arciiix <artur.nowak100@gmail.com>
2023-09-10 23:37:01 +0200

    Add text animation

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -5,6 +5,8 @@ import { useEffect, useMemo, useState } from "react";
 import FancyBackground from "../Animations/FancyBackground/FancyBackground";
 import AnimatedDeveloper from "../Animations/AnimatedDeveloper/AnimatedDeveloper";
 
+import styles from "./Landing.module.css";
+
 export default function Landing() {
   const [currentColor, setCurrentColor] = useState("#1baace");
   const lighterColor = useMemo(() => {
@@ -29,16 +31,20 @@ export default function Landing() {
       >
         <FancyBackground />
         <div className="my-24 h-full flex flex-col lg:flex-row items-center justify-center lg:justify-around">
-          <div className="m-16 mt-0 flex flex-col gap-4">
-            <span className="text-[100px] lg:text-[200px] font-extrabold">
-              I&apos;m{" "}
+          <div className="m-16 mt-0 flex flex-col gap-4 whitespace-pre">
+            <span className="text-[100px] lg:text-[200px] font-extrabold flex flex-col">
+              <span className="text-6xl">I&apos;m</span>
               <span
-                className="text-transparent bg-clip-text transition-all"
+                className="text-transparent bg-clip-text transition-all cursor-pointer"
                 style={{
                   backgroundImage: `linear-gradient(to right, ${currentColor}, ${lighterColor})`,
                 }}
               >
-                Artur
+                <span className={`${styles.letter} ${styles.letterA}`}>A</span>
+                <span className={`${styles.letter} ${styles.letterR}`}>r</span>
+                <span className={`${styles.letter} ${styles.letterT}`}>t</span>
+                <span className={`${styles.letter} ${styles.letterU}`}>u</span>
+                <span className={`${styles.letter} ${styles.letterR2}`}>r</span>
               </span>
             </span>
             <div className="flex flex-col gap-4">
